509fe330d0a4fc3e16762703de1283df06cde032
mickey mouse <mickey.mouse-1985@libero.it>
2014-03-22 21:15:25 +0100

    Added type mapping for tPageFormat and shapeType

---
+++ xml-schema/include/types/Graphics.hpp
@@ -0,0 +1,13 @@
+#ifndef TYPES_GRAPHICS_HPP
+#define TYPES_GRAPHICS_HPP
+
+namespace reportpp {
+
+enum class ShapeType {
+  RECTANGLE,
+  CIRCLE
+};
+
+} // namespace reportpp
+
+#endif /* ifndef TYPES_GRAPHICS_HPP */
---
+++ xml-schema/include/types/PageFormat.hpp
@@ -0,0 +1,23 @@
+#ifndef TYPES_REPORTPP_H
+#define TYPES_REPORTPP_H
+
+namespace reportpp {
+
+enum PageFormat {
+  letter,
+  legal,
+  a3,
+  a4,
+  a5,
+  b4,
+  b5,
+  executive,
+  us4x6,
+  us4x8,
+  us5x7,
+  comm10
+};
+
+} // namespace reportpp
+
+#endif /* ifndef TYPES_REPORTPP_H */
