a84403b43809a7f174bd7cede0e5c8e848334e27
mickey mouse <mickey.mouse-1985@libero.it>
2015-10-17 23:14:49 +0200

    added types_writer source

--- src/types/PageFormatParser.cpp
+++ src/types/PageFormatParser.cpp
@@ -24,25 +24,6 @@
 #include <iostream>
 #include <stdexcept>
 
-std::ostream& operator << (std::ostream& os, const reportpp::PageFormat& obj) {
-    switch (obj) {
-        case reportpp::PageFormat::letter: os << "reportpp::PageFormat::letter"; break;
-        case reportpp::PageFormat::legal: os << "reportpp::PageFormat::legal"; break;
-        case reportpp::PageFormat::a3: os << "reportpp::PageFormat::a3"; break;
-        case reportpp::PageFormat::a4: os << "reportpp::PageFormat::a4"; break;
-        case reportpp::PageFormat::a5: os << "reportpp::PageFormat::a5"; break;
-        case reportpp::PageFormat::b4: os << "reportpp::PageFormat::b4"; break;
-        case reportpp::PageFormat::b5: os << "reportpp::PageFormat::b5"; break;
-        case reportpp::PageFormat::executive: os << "reportpp::PageFormat::executive"; break;
-        case reportpp::PageFormat::us4x6: os << "reportpp::PageFormat::us4x6"; break;
-        case reportpp::PageFormat::us4x8: os << "reportpp::PageFormat::us4x8"; break;
-        case reportpp::PageFormat::us5x7: os << "reportpp::PageFormat::us5x7"; break;
-        case reportpp::PageFormat::comm10: os << "reportpp::PageFormat::comm10"; break;
-        default: os << "unknown!"; break;
-    }
-   return os;
-}
-
 namespace reportpp {
 
 PageFormat PageFormatParser::post_tPageFormat() {
